fb4a524d4be8e9cd84592fb412dd61a28f51e19d
khaoxkongz <thanapat.makkawal@gmail.com>
2024-02-20 13:21:05 +0700

    feat: favourite endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import * as RecipeAPI from "./recipe-api";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
-
+const prismaCilent = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
@@ -19,6 +20,52 @@ app.get("/api/recipes/:id/summary", async (req: Request, res: Response) => {
 	const recipeId = req.params.id;
 	const results = await RecipeAPI.getRecipeSummary(recipeId);
 	return res.json(results);
+});
+
+app.post("/api/recipes/favourite", async (req: Request, res: Response) => {
+	const recipeId = req.body.recipeId;
+
+	try {
+		const favouriteRecipe = await prismaCilent.favouriteRecipes.create({
+			data: {
+				recipeId: recipeId,
+			},
+		});
+		return res.status(201).json(favouriteRecipe);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Oops, something went worng" });
+	}
+});
+
+app.get("/api/recipes/favourite", async (req: Request, res: Response) => {
+	try {
+		const recipe = await prismaCilent.favouriteRecipes.findMany();
+		const recipeIds = recipe.map((recipe) => recipe.recipeId.toString());
+
+		const favourites = await RecipeAPI.getFavouriteRecipesByIDs(recipeIds);
+
+		return res.json(favourites);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Oops, something went worng" });
+	}
+});
+
+app.delete("/api/recipes/favourite", async (req, res) => {
+	const recipeId = req.body.recipeId;
+
+	try {
+		await prismaCilent.favouriteRecipes.delete({
+			where: {
+				recipeId: recipeId,
+			},
+		});
+		return res.status(204).send();
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error: "Oops, something went worng" });
+	}
 });
 
 app.listen(8888, () => {
